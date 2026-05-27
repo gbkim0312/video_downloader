e4066475f3d7dd043d34e8f258592d0b3e88f6d3
Gibeom Kim <gbkim@autocrypt.io>
2026-05-28 08:34:23 +0900

    Remove legacy compatibility modules

--- video_downloader/browser_protection.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from .adapters.browser.protection import (
-    AD_POPUP_HINTS,
-    install_popup_protection,
-    looks_like_ad_popup_url,
-)
-
-__all__ = ["AD_POPUP_HINTS", "install_popup_protection", "looks_like_ad_popup_url"]
--- video_downloader/models.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-from .domain.models import BatchJob, BatchSummary, LinkCandidate, StreamCandidate
-
-__all__ = ["BatchJob", "BatchSummary", "LinkCandidate", "StreamCandidate"]
